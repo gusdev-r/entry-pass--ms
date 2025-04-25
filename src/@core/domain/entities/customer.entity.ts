8cac1e100a124d4877757cce44a7bfd36ef886c1
Gustavo Moreira <g.hen.moreira@gmail.com>
2025-04-25 20:47:41 -0300

    base: nest started project files

---
+++ src/@core/domain/entities/customer.entity.ts
@@ -0,0 +1,27 @@
+export type CustomerConstructorProps = {
+  id?: string;
+  cpf: string;
+  name: string;
+};
+
+export class Customer {
+  id: string;
+  cpf: string;
+  name: string;
+
+  constructor(props: CustomerConstructorProps) {
+    this.id = props.id;
+    this.cpf = props.cpf;
+    this.name = props.name;
+  }
+  static create(command: { name: string; cpf: string }): Customer {
+    return new Customer(command);
+  }
+  toJson() {
+    return {
+      id: this.id,
+      cpf: this.cpf,
+      name: this.name,
+    };
+  }
+}
